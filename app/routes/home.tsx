c26136b347a7b83536c7ed1c15a6c8b3b10cd066
m-nakamine <kawarabasami.dev@gmail.com>
2025-07-19 10:42:50 +0900

    biome設定追加

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,18 +1,19 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Welcome } from '../welcome/welcome';
+import type { Route } from './+types/home';
 
 export const loader = () => {
-  console.log('loder called!')
-}
+  console.log('loder called!');
+};
 
+// biome-ignore lint/correctness/noEmptyPattern: <explanation>
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: 'New React Router App' },
+    { name: 'description', content: 'Welcome to React Router!' },
   ];
 }
 
 export default function Home() {
-  console.log('home called!')
+  console.log('home called!');
   return <Welcome />;
 }
