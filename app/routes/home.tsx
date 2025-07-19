6793c20c179937c46070b704518bb1ad40c3cccb
m-nakamine <kawarabasami.dev@gmail.com>
2025-07-19 10:26:29 +0900

    デバッグ設定の追加

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,10 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
+export const loader = () => {
+  console.log('loder called!')
+}
+
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
@@ -9,5 +13,6 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  console.log('home called!')
   return <Welcome />;
 }
